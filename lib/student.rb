ed373c797258fd1e0c59d6e1e8db8176f8542547
Will Hodgson <will.hodgson@current-rms.com>
2019-10-02 12:33:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,9 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-    DB[:conn].execute("SELECT * FROM students")
+    DB[:conn].execute("SELECT * FROM students").map do |student|
+
+    end
   end
 
   def self.find_by_name(name)
