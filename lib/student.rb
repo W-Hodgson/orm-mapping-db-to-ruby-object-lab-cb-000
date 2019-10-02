469a5ecb3720c4785cd87bf34e4c188611c7dfc0
Will Hodgson <will.hodgson@current-rms.com>
2019-10-02 12:30:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,9 +4,9 @@ class Student
   def self.new_from_db(row)
     # create a new Student object given a row from the database
     student = Student.new
-    student.id = row.id
-    student.name = row.name
-    student.grade = row.grade
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
   end
 
   def self.all
